7aa369e71c471c786aae3fff8a6163251dfb2a8d
29rosebud <silver29rosebud@yahoo.com>
2017-05-23 13:54:25 -0400

    Update Finalclass.java

--- Finalclass.java
+++ Finalclass.java
@@ -10,15 +10,22 @@ public class Finalclass
 
 	public void draw(Graphics g)
 	{	g.setColor(Color.WHITE);
-		g.fillOval(177,639,150,100);
-		g.fillOval(160,421,150,100);
+		g.fillOval(135,600,150,100);
+		g.setColor(Color.RED);
+		g.fillOval(135,410,150,100);
+		g.setColor(Color.MAGENTA);
 		g.fillOval(135,190,150,100);
-		g.fillOval(462,190,150,100);
-		g.fillOval(488,407,150,100);
-		g.fillOval(490,603,150,100);
-		g.fillOval(842,606,150,100);
-		g.fillOval(787,353,150,100);
-		g.fillOval(790,181,150,100);
+		g.setColor(Color.CYAN);
+		g.fillOval(488,190,150,100);
+		g.setColor(Color.BLUE);
+		g.fillOval(488,410,150,100);
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(490,600,150,100);
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillOval(790,600,150,100);
+		g.setColor(Color.GREEN);
+		g.fillOval(790,410,150,100);
+		g.setColor(Color.YELLOW);
 	}
 /// 177,639   160,421,    135,196    462,190
 //488, 407         490,603      842,606     787,353     790,181
