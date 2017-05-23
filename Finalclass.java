96deb57399221f13736783effea5bf83f109d237
29rosebud <silver29rosebud@yahoo.com>
2017-05-23 13:36:04 -0400

    Add files via upload

---
+++ Finalclass.java
@@ -0,0 +1,28 @@
+//Skylar James paul
+//final
+//5/22/17
+import java.awt.*;
+
+public class Finalclass
+{
+
+
+
+	public void draw(Graphics g)
+	{	g.setColor(Color.WHITE);
+		g.fillOval(177,639,150,100);
+		g.fillOval(160,421,150,100);
+		g.fillOval(135,190,150,100);
+		g.fillOval(462,190,150,100);
+		g.fillOval(488,407,150,100);
+		g.fillOval(490,603,150,100);
+		g.fillOval(842,606,150,100);
+		g.fillOval(787,353,150,100);
+		g.fillOval(790,181,150,100);
+	}
+/// 177,639   160,421,    135,196    462,190
+//488, 407         490,603      842,606     787,353     790,181
+
+
+
+}
