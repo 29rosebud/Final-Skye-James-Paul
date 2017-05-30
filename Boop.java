fe1f67d922bf6591bec7dead00e424eccb535741
DabMaster420 <corbett0823@gmail.com>
2017-05-30 13:44:10 -0400

    Update Boop.java

--- Boop.java
+++ Boop.java
@@ -9,11 +9,19 @@ import java.awt.event.*;
 import java.applet.*;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.util.Random;
 
 
-public class Boop extends JApplet implements ActionListener, KeyListener
+public class Driver extends JApplet implements ActionListener, KeyListener,MouseListener,MouseMotionListener
 {
 		Timer time;
+		int XClick;
+		int YClick;
+		int X;
+		int Y;
+		//TEST Image i;
+		Finalclass hog;
+		Random r;
 
   public void actionPerformed(ActionEvent e)
   {
@@ -33,6 +41,39 @@ public class Boop extends JApplet implements ActionListener, KeyListener
 
 
     }
+    public void mouseClicked(MouseEvent e)
+			{
+
+
+			}
+		public void mousePressed(MouseEvent e)
+			{
+
+			}
+
+		public void mouseReleased(MouseEvent e)
+			{
+
+			}
+
+		public void mouseEntered(MouseEvent e)
+			{
+
+			}
+
+		public void mouseExited(MouseEvent e)
+			{
+
+			}
+		public void mouseMoved(MouseEvent e)
+			{
+			//	XClick= e.getX()-X; for hammer?
+			//	YClick= e.getY()-Y;
+
+			}
+	public void mouseDragged(MouseEvent e)
+		{
+		}
 		public void init()
 		{
 				setFocusable(true);
@@ -40,6 +81,10 @@ public class Boop extends JApplet implements ActionListener, KeyListener
 				Container c=getContentPane();
 				c.setLayout(new FlowLayout());
 			addKeyListener(this);
+				hog=new Finalclass();
+				r= new Random();
+				c.setBackground(Color.BLACK);
+			// TEST IMAGE i = getImage(getDocumentBase( ),"birb.jpg");
 
 		}
 		public class drawingPanel extends JPanel
@@ -47,8 +92,8 @@ public class Boop extends JApplet implements ActionListener, KeyListener
 				public void paintComponent(Graphics g)
 				{
 					super.paintComponent(g);
-
-
+				//TEST	g.drawImage(i,50, 50, this);
+					hog.draw(g);
 				}
 
 			}
