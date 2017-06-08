4f55e4eff464ea583bf57a57332a9643c74fd676
29rosebud <silver29rosebud@yahoo.com>
2017-06-08 14:08:39 -0400

    Update seal.java

--- seal.java
+++ seal.java
@@ -4,46 +4,85 @@ import java.util.Random;
 
 public class seal
 {
- Image sad,happy,current;
- int x,y;
- boolean circ;
- Random r=new Random();
- int circle1;
+	Image sad,happy,current;
+	int x,y;
+	boolean circ;
+	Random r=new Random();
+	int circle1;
+	boolean check;
+	public seal(Image happy,Image sad )
+	{
+		this.happy=happy;
+		this.sad=sad;
+		//this.x=x;
+		//this.y=y;
+		current=happy;
+	}
 
- public seal(Image happy,Image sad,int x,int y )
- {
-  this.happy=happy;
-  this.sad=sad;
-  this.x=x;
-  this.y=y;
-  current=happy;
- }
+	public void draw(Graphics g)
+	{
+		g.drawImage(current,x,y,null);
+	}
+	public void swap()
+	{
+		if(current==happy)
+		{
+			current=sad;
+			System.out.println("sad");
+		}
+		else
+		{
+			current=happy;
+			System.out.println("happy");
+		}
+	}
+	public void sad()
+	{
+		current=sad;
+	}
+	public void happy()
+	{
+		current=happy;
+	}
+	public boolean sealCheck()
+		{
+			circ=false;
+			circle1=r.nextInt(100);
+			if(circle1 >= 90)
+			{
+				circ=true;
+			}
+			return circ;
+		}
+		public boolean coordCheck(int x3, int y3)
+		{
+					//System.out.println("p x " + x + "y " + y + "x3 " + x3 + "y3 " + y3);
+			check=false;
+			if(x3 <= x +73 && x3 >= x)
+			{
+				if(y3 >= y && y3 <= y + 63)
+				{
+					//if(x3 > x && x3 < (x+73) && y3 >y && y3 < (y+63))
+					//{
+					check=true;
 
- public void draw(Graphics g,int x1,int y1)
- {
-  g.drawImage(current,x1,y1,null);
- }
- public void swap()
- {
-  if(current==happy)
-  {
-   current=sad;
-  }
-  else if(current==sad)
-  {
-   current=happy;
-  }
- }
- public boolean sealCheck()
-  {
-   circ=false;
-   circle1=r.nextInt(100);
-   if(circle1 >= 90)
-   {
-    circ=true;
-   }
-   return circ;
-  }
+					//System.out.println("x " + x + "y " + y + "x3 " + x3 + "y3 " + y3);
+
+					//}
+				}
+
+			}
+			return check;
+		}
+		public void setX(int xx)
+		{
+			x=xx;
+		}
+
+		public void setY(int yy)
+		{
+			y=yy;
+		}
 
 
 }
